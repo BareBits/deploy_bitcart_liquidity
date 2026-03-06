60e2b5a1d13328f923ecc98e905e762da4486d09
nothing-stops-this-train <andrew@getbarebits.com>
2026-03-05 18:48:44 -0800

    Update update_liquidityhelper.sh for cleaner output
    Suppress output of git pull command and add echo message for no updates.

--- update_liquidityhelper.sh
+++ update_liquidityhelper.sh
@@ -2,13 +2,14 @@
 set -e
 cd bitcart_liquidity 
 old_head=$(git rev-parse HEAD)
-git pull
+git pull >> /dev/null
 new_head=$(git rev-parse HEAD)
 
+#date
 if [ "$old_head" = "$new_head" ]; then
-    # no updates
+    echo "no updates for lhelper found"
     exit 0
 else
-    # updated so we must restart
+    echo "liquidityhelper updated!"
     systemctl restart liquidityhelper
 fi
